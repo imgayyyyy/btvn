99cde11f26e23438472ac9c88e5367e7312702f4
iamgayyyyy <pdatk881@gmail.com>
2024-10-02 02:56:57 +0700

    btvn1

---
+++ FPLF24DL B24DTCN211 AS0101.c.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    printf("pham tien dat - B24DTCN211\n");
+    printf("pham\n");
+    printf("tien\n");
+    printf("dat\n");
+    printf("B24DTCN211\n");
+    return 0;
+}
